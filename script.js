d97e02cae04a4f89eaec643311bcba1faddb464b
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-01-08 11:52:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var _frame = document.getElementById('frame');
+(function () {
+  var _frame = document.getElementById('frame');
 
-var frame = _frame.contentDocument;
-
-var body = frame.children[0].children[1];
-var head = frame.children[0].children[0];
-var style = document.createElement('style');
-head.appendChild(style);
-var script = document.createElement('script');
-head.appendChild(script);
-
-document.onkeyup = function() {
-  body.innerHTML = document.querySelector('.html').innerHTML;
-  style.innerHTML = document.querySelector('.css').innerHTML;
-  script.innerHTML = document.querySelector('.js').innerHTML;
-}
-
+  var frame = _frame.contentDocument;
+  
+  var body = frame.children[0].children[1];
+  var head = frame.children[0].children[0];
+  var style = document.createElement('style');
+  head.appendChild(style);
+  var script = document.createElement('script');
+  head.appendChild(script);
+  
+  document.onkeyup = function() {
+    body.innerHTML = document.querySelector('.html').innerHTML;
+    style.innerHTML = document.querySelector('.css').innerHTML;
+    script.innerHTML = document.querySelector('.js').innerHTML;
+  }
+}());
