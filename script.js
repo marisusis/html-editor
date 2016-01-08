b3d1a58b1aa6bd198488c0023cf851dac289e25a
spacegeek224 <spacegeek224@users.noreply.github.com>
2016-01-08 09:24:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ var _frame = document.getElementById('frame');
 var frame = _frame.contentDocument;
 
 var body = frame.children[0].children[1];
+var head = frame.children[0].children[0];
+
+var script = head.createElement('style');
 
 document.onkeyup = function() {
   body.innerHTML = document.querySelector('.html').innerHTML;
+  script.innerHTML = document.querySelector('.css').innerHTML;
 }
